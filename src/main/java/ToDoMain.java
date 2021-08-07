d28a92ea64f06cfc6818217c67149c45f33d9042
Matin <matin.parsapour.iam@gmail.com>
2021-08-07 18:20:47 +0430

    update main

--- src/main/java/ToDoMain.java
+++ src/main/java/ToDoMain.java
@@ -1,2 +1,11 @@
+import util.HibernateUtil;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
 public class ToDoMain {
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = HibernateUtil.getEntityManagerFactory();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+    }
 }
