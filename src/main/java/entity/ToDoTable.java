b248b49a81ab5e6bcd745285321863b2bcfb62ce
Matin <matin.parsapour.iam@gmail.com>
2021-08-07 18:13:43 +0430

    create todotable class methods

--- src/main/java/entity/ToDoTable.java
+++ src/main/java/entity/ToDoTable.java
@@ -1,4 +1,64 @@
 package entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
 public class ToDoTable {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "body")
+    private String body;
+
+    @Column(name = "creationdate")
+    private LocalDate creationDate;
+
+    public ToDoTable(String title,String body, LocalDate creationDate){
+        this.title = title;
+        this.body = body;
+        this.creationDate = creationDate;
+    }
+
+    public ToDoTable(){
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
 }
