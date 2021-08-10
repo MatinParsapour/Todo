7b1b8269dd15fe7d5285328f87c04f27e2ccab28
Matin <matin.parsapour.iam@gmail.com>
2021-08-10 07:39:35 +0430

    recreate last custom linked list

---
+++ src/main/linkedlist/CustomLinkedListMain.java
@@ -0,0 +1,71 @@
+package linkedlist;
+
+
+public class CustomLinkedListMain<T> {
+
+    private CustomNode start;
+    public int length;
+
+    public boolean isEmpty() {
+        return start == null;
+    }
+
+    public void insertNode(T information) {
+        CustomNode recentNode = new CustomNode(information);
+        if (start == null) {
+            start = recentNode;
+        } else {
+            CustomNode actualNode = start;
+            while (actualNode.getNextNode() != null) {
+                actualNode = actualNode.getNextNode();
+            }
+            actualNode.setNextNode(recentNode);
+        }
+        length++;
+    }
+
+    public void deleteNode(int index) {
+        CustomNode customNode = start;
+        if(index == 0){
+            start = start.getNextNode();
+        }
+        else{
+            for (int counter = 0; counter < index - 1; counter++) {
+                customNode = customNode.getNextNode();
+            }
+            customNode.getNextNode().setNextNode(customNode.getNextNode().getNextNode());
+        }
+        length--;
+    }
+    public int indexOf(T element){
+        int index = -1;
+        CustomNode currentNode = start;
+        if(isEmpty()){
+            throw new IndexOutOfBoundsException("you have nothing here");
+        }
+        int temp = 0;
+        while(currentNode != null){
+            if(currentNode.getInformation() == element){
+                index = temp;
+                break;
+            }
+            currentNode = currentNode.getNextNode();
+            temp++;
+        }
+        return index;
+    }
+    @Override
+    public String toString(){
+        String toString = "(";
+        if(start != null){
+            CustomNode currentNode = start;
+            while(currentNode != null){
+                toString += "[";
+                toString += currentNode.getInformation() + "]";
+                currentNode = currentNode.getNextNode();
+            }
+        }
+        toString += ")";
+        return toString;
+    }
+}
---
+++ src/main/linkedlist/CustomNode.java
@@ -0,0 +1,26 @@
+package linkedlist;
+
+public class CustomNode<T> {
+    private T information;
+    private CustomNode nextNode;
+
+    public CustomNode(T information){
+        this.information = information;
+    }
+
+    public T getInformation() {
+        return information;
+    }
+
+    public void setInformation(T information) {
+        this.information = information;
+    }
+
+    public CustomNode getNextNode() {
+        return nextNode;
+    }
+
+    public void setNextNode(CustomNode nextNode) {
+        this.nextNode = nextNode;
+    }
+}
