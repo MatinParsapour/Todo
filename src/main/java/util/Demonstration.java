8cbad16c16231e84aafe62126ece859d0d723930
Matin <matin.parsapour.iam@gmail.com>
2021-08-14 18:53:21 +0430

    create demonstration class in order to print menus

---
+++ src/main/java/util/Demonstration.java
@@ -0,0 +1,11 @@
+package util;
+
+public class Demonstration {
+    public void mainMenu(){
+        System.out.println("-----------------------");
+        System.out.println("       1.Sign Up       ");
+        System.out.println("        2.Log In       ");
+        System.out.println("         3.Exit        ");
+        System.out.println("-----------------------");
+    }
+}
