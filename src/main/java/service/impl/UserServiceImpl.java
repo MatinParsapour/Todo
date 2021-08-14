306e4eb3d71568ed0f41bba8a6b1eef0f203fab4
Matin <matin.parsapour.iam@gmail.com>
2021-08-14 20:15:43 +0430

    create primary menu for users to do their works

--- src/main/java/service/impl/UserServiceImpl.java
+++ src/main/java/service/impl/UserServiceImpl.java
@@ -7,6 +7,7 @@ import repository.UserRepository;
 import service.UserService;
 import util.ApplicationContext;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class UserServiceImpl extends BaseEntityServiceImpl<User,Long, UserRepository> implements UserService {
@@ -17,27 +18,54 @@ public class UserServiceImpl extends BaseEntityServiceImpl<User,Long, UserReposi
 
     @Override
     public void logIn(){
-        while(true){
-            Scanner input = new Scanner(System.in);
-            System.out.print("username : ");
-            String username = input.next();
-            System.out.print("password : ");
-            String password = input.next();
-            if(ApplicationContext.getUserServiceImpl().repository.checkUser(username,password)){
-                System.out.println("<><><> You logged in <><><>");
-                ApplicationContext.getDemonstration().choicesMenu();
-            }else{
-                System.out.println("Your username or password is incorrect");
-                System.out.println("1.Back to main menu        2.Try again");
-                int choice = new Scanner(System.in).nextInt();
-                while(choice < 1 || choice > 2){
-                    System.out.println("You should choose 1 or 2");
-                    System.out.println("Try again");
-                    choice = new Scanner(System.in).nextInt();
+        boolean exit = false;
+        while(!exit){
+            try{
+                Scanner input = new Scanner(System.in);
+                System.out.print("username : ");
+                String username = input.next();
+                System.out.print("password : ");
+                String password = input.next();
+                if(ApplicationContext.getUserServiceImpl().repository.checkUser(username,password)){
+                    System.out.println("<><><> You logged in <><><>");
+                    while(true){
+                        ApplicationContext.getDemonstration().choicesMenu();
+                        int choice = new Scanner(System.in).nextInt();
+                        if(choice == 1){
+                            //see your activities
+                        }else if (choice == 2){
+                            //add an activity
+                        }else if (choice == 3){
+                            //change status
+                        }else if(choice == 4){
+                            //change password
+                        }else if(choice == 5){
+                            //change username
+                        }else if(choice == 6){
+                            //change activity title
+                        }else if(choice == 7){
+                            //change activity content
+                        }else if(choice == 8){
+                            exit = true;
+                            break;
+                        }
+                    }
+                }else{
+                    System.out.println("Your username or password is incorrect");
+                    System.out.println("1.Back to main menu        2.Try again");
+                    int failedLogInChoice = new Scanner(System.in).nextInt();
+                    while(failedLogInChoice < 1 || failedLogInChoice > 2){
+                        System.out.println("You should choose 1 or 2");
+                        System.out.println("Try again");
+                        failedLogInChoice = new Scanner(System.in).nextInt();
+                    }
+                    if(failedLogInChoice == 1){
+                        exit = true;
+                    }
                 }
-                if(choice == 1){
-                    break;
-                }
+            }catch (InputMismatchException exception){
+                System.out.println("You should enter number");
+                System.out.println("Start over");
             }
         }
     }
