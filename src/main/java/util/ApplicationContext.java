567d2871a5c892837d91a38f08cf42514d442a05
Matin <matin.parsapour.iam@gmail.com>
2021-08-15 14:13:34 +0430

    update ApplicationContext

--- src/main/java/util/ApplicationContext.java
+++ src/main/java/util/ApplicationContext.java
@@ -1,7 +1,10 @@
 package util;
 
+import repository.TaskRepository;
 import repository.UserRepository;
+import repository.impl.TaskRepositoryImpl;
 import repository.impl.UserRepositoryImpl;
+import service.impl.TaskServiceImpl;
 import service.impl.UserServiceImpl;
 
 import javax.persistence.EntityManager;
@@ -10,6 +13,10 @@ public class ApplicationContext {
     private static final UserRepository userRepository;
 
     private static final UserServiceImpl userServiceImpl;
+
+    private static final TaskRepository taskRepository;
+
+    private static final TaskServiceImpl taskServiceImpl;
 
     private static final Demonstration demonstration;
 
@@ -20,6 +27,8 @@ public class ApplicationContext {
         EntityManager entityManager = HibernateUtil.getEntityManagerFactory().createEntityManager();
         userRepository = new UserRepositoryImpl(entityManager);
         userServiceImpl = new UserServiceImpl(userRepository);
+        taskRepository = new TaskRepositoryImpl(entityManager);
+        taskServiceImpl = new TaskServiceImpl(taskRepository);
         demonstration = new Demonstration();
     }
     public static UserServiceImpl getUserServiceImpl(){
@@ -27,4 +36,6 @@ public class ApplicationContext {
     }
 
     public static Demonstration getDemonstration(){return demonstration;}
+
+    public static TaskServiceImpl getTaskServiceImpl(){return taskServiceImpl;}
 }
